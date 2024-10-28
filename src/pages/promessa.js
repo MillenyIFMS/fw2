cc65267a0d811039bc9cfdfa55d3fa0e8323f6b2
MillenyIFMS <milleny.xavier@estudante.ifms.edu.br>
2024-10-28 15:28:34 +0000

    aula dia 28/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,20 +1,34 @@
 import { Container } from "react-bootstrap";
 import Menu from "./component/navbar";
 import Footer from "./component/footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Promessa(){
+
+
     const [prometido, setPrometido] = useState("")
+    useEffect(() => { 
     setTimeout(() => {
-         setPrometido("é uma divida")
+        setPrometido(mudaTexto("Ola Texto novo"));
         document.title="Promessa é divida"
-    }, 2000);
+    }, 4000);
+    });
     return<>
     <Menu /> 
     <Container>
-        Promessa {prometido}
+     Promessa {prometido === "" ? <img src="./gif fw2.gif" width={200} height={100}/> : prometido}
     </Container>
     <Footer />
     </>
 }
+
+function mudaTexto(texto) {
+    return new Promisse ((resolva) => 
+        setTimeout(() => {
+         resolva(texto);
+
+    }, 1000)
+
+)
+};
